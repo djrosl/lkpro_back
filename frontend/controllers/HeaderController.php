ff42bc3fe05b87f321c9b2f500cf068db665b441
rosl <dubztep45@gmail.com>
2016-04-11 14:14:57 +0300

    fix

--- frontend/controllers/HeaderController.php
+++ frontend/controllers/HeaderController.php
@@ -7,10 +7,10 @@ use yii\rest\ActiveController;
 class HeaderController extends ActiveController
 {
 	public function cors() {
-
+        header("Access-Control-Allow-Origin: '*'");
         // Allow from any origin
         if (isset($_SERVER['HTTP_ORIGIN'])) {
-            header("Access-Control-Allow-Origin: '*'");
+            
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Max-Age: 86400');    // cache for 1 day
         }
