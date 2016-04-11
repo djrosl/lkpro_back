6adbeab14607c279a5a9601444318ffa11c9cc6d
rosl <dubztep45@gmail.com>
2016-04-11 14:35:09 +0300

    fix

--- frontend/controllers/HeaderController.php
+++ frontend/controllers/HeaderController.php
@@ -30,27 +30,12 @@ class HeaderController extends ActiveController
 
 
     public function behaviors(){
-        //$this->cors();
-        /*$behaviors = parent::behaviors();
+        $this->cors();
+        $behaviors = parent::behaviors();
 
 
-        return $behaviors;*/
-        return [
-            'corsFilter' => [
-                'class' => \yii\filters\Cors::className(),
-                'cors' => [],
-                'actions' => [
-                    'incoming' => [
-                        'Origin' => ['*'],
-                        'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                        'Access-Control-Request-Headers' => ['*'],
-                        'Access-Control-Allow-Credentials' => null,
-                        'Access-Control-Max-Age' => 86400,
-                        'Access-Control-Expose-Headers' => [],
-                    ],
-                ],
-            ],
-        ];
+        return $behaviors;
+        
     }
 
 
